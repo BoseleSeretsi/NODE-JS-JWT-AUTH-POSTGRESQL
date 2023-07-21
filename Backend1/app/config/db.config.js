08fdacd6d1380f0c0a6aa8963e8755295128f323
BoseleSeretsi <129956765+BoseleSeretsi@users.noreply.github.com>
2023-07-21 16:28:38 +0200

    initial

---
+++ Backend1/app/config/db.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    HOST: "dpg-chvj30pmbg5b5p91k7f0-a.oregon-postgres.render.com",
+    USER: "dbproduct_sztw_user",
+    PASSWORD: "GGig08eYe0kMLpMMtzZ7wvo2uJX4A9yg",
+    DB: "dbproduct_sztw",
+    dialect: "postgres",
+    ssl:true,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+
+
+  //postgres://dbproduct_sztw_user:GGig08eYe0kMLpMMtzZ7wvo2uJX4A9yg@dpg-chvj30pmbg5b5p91k7f0-a.oregon-postgres.render.com/dbproduct_sztw
